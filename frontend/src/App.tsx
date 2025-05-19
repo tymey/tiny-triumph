ef1cb02e4afd4dcc709c506353f2329ca912ffd7
tymey <tyler.meyer.a@gmail.com>
2025-05-18 22:22:04 -0500

    build: Project structure & source files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Tiny Triumphs</h1>
+            <p>Your journey to celebrate every small win begins here.</p>
+        </div>
+    );
+};
+
+export default App;
